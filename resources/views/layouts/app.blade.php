a326ca131fb697e418c3da67f4d6060a63a35a8f
mmanov <mmanov@adsterra.com>
2024-11-25 20:34:57 +0300

    add controller, route, models and some front

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Маршруты автобусов</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <script src="{{ asset('js/app.js') }}"></script>
+    </head>
+    <body>
+        <div class="container mt-4">
+            <a></a>
+            <a></a>
+        </div>
+        <br>
+        <div class="container mt-4">
+            @yield('content')
+        </div>
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,1 @@
-<form action="/api/find-bus" method="post">
-    <div>
-        <label>
-            From
-            <input type="text" name="destination[from]" value="<?= htmlspecialchars($destination['from']); ?>">
-        </label>
-        <label>
-            To
-            <input type="text" name="destination[to]" value="<?= htmlspecialchars($destination['to']); ?>">
-        </label>
-    </div>
-    <input type="submit" value="Find">
-</form>
+@extends('layouts.app')
